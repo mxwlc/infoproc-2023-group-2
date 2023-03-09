71c44d087dd6b382ab467ecb88a859ac59dadcd9
Matthew Stevenson <matthew.stevenson21@imperial.ac.uk>
2023-03-09 10:16:16 +0000

    edited for more simple multiplayer

--- sendclientdata/clientsendplayer1.py
+++ sendclientdata/clientsendplayer1.py
@@ -17,35 +17,35 @@ client_socket.connect((server_name, server_port))
 
 while True:
 
-    #conditional updates - only send update to server if change is registered, to save latency as these dont need to be sent 10 times a second if theyre usually the same
-    if player1_lives == player1_lives-1:
-        client_socket.send(player_lives.encode())
+    #conditional updates - only send update to server if change is registered, to save latency as these dont need to be sent 10 times a second if theyre usually the same:
+    client_socket.send(player1_lives.encode())
     
-    else if base_lives == base_lives-1:
-        client_socket.send(base_lives.encode())
+    client_socket.send(player1_score.encode())
+    #else if base_lives == base_lives-1:
+      #  client_socket.send(base_lives.encode())
 
-    else if #space invader is destroyed:
-        client_socket.send(#space invader is destroyed.encode())
+    #else if #space invader is destroyed:
+       # client_socket.send(#space invader is destroyed.encode())
     
-    else if #bullet shot:
-        client_socket.send(#bullet shot.encode())
+    #else if #bullet shot:
+       # client_socket.send(#bullet shot.encode())
     
 
     #concatenate automatically sent game data into a string
-    auto_game_data = f'{player1_x_position}|{player1_score}'
+    #auto_game_data = f'{player1_x_position}|{player1_score}'
 
     # send variable values to server
-    client_socket.send(auto_game_data.encode())
+    #client_socket.send(auto_game_data.encode())
 
 
     #get updates on all non local variables from server
     player2_lives = client_socket.recv(1024).decode()
     player2_score = client_socket.recv(1024).decode()
-    player2_x_position = client_socket.recv(1024).decode()
+    #player2_x_position = client_socket.recv(1024).decode()
 
 
     
     #wait a small while before sending more data. this will send info about users actions 10 times per second.
-    time.sleep(0.1)
+    time.sleep(0.05)
 
                                                                                                                                                                 
