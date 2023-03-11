160ba62d887ffec5b90d8a7631463e40303b7b15
mxwlc <maxwellcorey85@gmail.com>
2023-03-11 13:52:26 +0000

    added documentation

--- fpga-to-xinput/controller.py
+++ fpga-to-xinput/controller.py
@@ -6,38 +6,104 @@ import time
 
 
 class fpga_controller:
+    """
+    Controller Class used to control the movements of the player
+        ...
+
+        Attributes
+
+        ----------
+
+        keyboard : Controller Object
+            Simulates key presses and releases to control the player
+
+
+        Methods
+
+        -------
+
+        shoot()
+            Shoots a projectile
+
+        moveleft()
+            moves the player left
+
+        moveright()
+            moves the player right
+
+        stop()
+            stops the player from moving
+
+        left_or_right(accel_input)
+            decides whether the player should move left or right
+            parameters:
+                accel_input : raw hex output of the fpga accelerometer
+
+        uhex_to_shex(uhex)
+            converts the raw hex output from unsigned hex to signed decimal
+            parameters:
+                uhex : raw hex output from accelerometer
+    """
 
     def __init__(self):
+        """
+        initialises the keyboard by creating a controller object
+        """
         self.keyboard = Controller()
 
     def shoot(self):
-        # TODO: implement space
+        """
+        Player shoots
+        """
         self.keyboard.press(Key.space)
         time.sleep(0.1)
         self.keyboard.release(Key.space)
 
     def moveleft(self):
-        # TODO: implement move left
+        """
+        Moves the player to the left by simulating the pressing of a left key
+        """
         self.keyboard.press(Key.left)
 
     def moveright(self):
-        # TODO: implement move right
+        """
+        Moves the player to the right by simulating the pressing of a right key
+        """
         self.keyboard.press(Key.right)
 
     def stop(self):
+        """
+        Stops the player from moving at all by "releasing" both buttons
+        """
         self.keyboard.release(Key.left)
         self.keyboard.release(Key.right)
 
     def left_or_right(self, accel_input):
+        """
+        Moves the player left or right based on the readings from the fpga
+        TODO: Tune the thresholds so that the controller works as intended
+
+        ...
+         Parameters:
+         -----------
+            accel_input : raw hex readings from the fpga
+        """
         accel_input = int(self.uhex_to_shex(accel_input))
-        if accel_input < -100:
+        if accel_input < -10:
             self.moveleft()
-        elif accel_input > 100:
+        elif accel_input > 10:
             self.moveright()
         else:
             self.stop()
 
     def uhex_to_shex(uhex):
+        """
+        Converts the unsigned 32-bit hex data to signed 32-bit decimal
+        ...
+        Parameters
+        ----------
+            uhex : unsigned 32-bit Hexidecimal
+        """
         if uhex & 0x80000000:
             return - (uhex & 0x79999999)
         else:
