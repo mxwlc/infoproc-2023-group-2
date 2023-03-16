85af59c7dc77be720244d5f0f067526fa55b2d82
James Donald <dev@chaoscantrip.com>
2023-03-16 12:49:15 +0000

    Chanced C code to display lives

--- UART/src/software/langproc/hello_world_small.c
+++ UART/src/software/langproc/hello_world_small.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include "sys/alt_stdio.h"
+#include "altera_avalon_pio_regs.h"
+#include "system.h"
+#include "altera_up_avalon_accelerometer_spi.h"
 #define CHARLIM 256		// Maximum character length of what the user places in memory.  Increase to allow longer sequences
 #define QUITLETTER '~' 		// Letter to kill all processing
 
@@ -25,13 +29,24 @@ int read_chars() {
 	int length = 0;
 	int running = 1;
 
+	int lives = 0;
+	volatile int * BTN_ptr = (int *)BUTTON_BASE;
+	volatile int * SW_ptr = (int *)SWITCH_BASE;
+    alt_32 x_read;
+    alt_up_accelerometer_spi_dev * acc_dev;
+    acc_dev = alt_up_accelerometer_spi_open_dev("/dev/accelerometer_spi");
+    if (acc_dev == NULL) { // if return 1, check if the spi ip name is "accelerometer_spi"
+        return 1;
+    }
+
 	while (running) {									// Keep running until QUITLETTER is encountered
 		prevLetter = alt_getchar();							// Extract the first character (and create a hold until one arrives)
 		prevLetter = get_input(prevLetter, &length, text, &running);		// Process input text
 		switch (text[0]){
 
 		case 'r':
-			alt_printf("Read");
+			alt_up_accelerometer_spi_read_x_axis(acc_dev, & x_read);
+			alt_printf("%x|%x|%x", *(BTN_ptr), *(SW_ptr), x_read);
 			break;
 
 		case 's':
@@ -39,7 +54,40 @@ int read_chars() {
 			break;
 
 		case 'l':
-			alt_printf("Lives");
+			switch(text[2]){
+			case '1':
+				lives = 0b1;
+				break;
+			case '2':
+				lives = 0b11;
+				break;
+			case '3':
+				lives = 0b111;
+				break;
+			case '4':
+				lives = 0b1111;
+				break;
+			case '5':
+				lives = 0b11111;
+				break;
+			case '6':
+				lives = 0b111111;
+				break;
+			case '7':
+				lives = 0b1111111;
+				break;
+			case '8':
+				lives = 0b11111111;
+				break;
+			case '9':
+				lives = 0b111111111;
+				break;
+			default:
+				lives = 0b0;
+				break;
+			}
+			alt_printf("Lives %s", &text[2]);
+			IOWR_ALTERA_AVALON_PIO_DATA(LED_BASE, lives);
 			break;
 
 		default:
