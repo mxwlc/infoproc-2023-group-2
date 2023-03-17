f9ff0b9e83b1a319604b720c22fdf60e33990ad6
James Donald <dev@chaoscantrip.com>
2023-03-17 14:46:46 +0000

    Updated FPGAController to use Inputs class

--- UART/host.py
+++ UART/host.py
@@ -1,5 +1,20 @@
 import intel_jtag_uart as _uart
 
+_BUTTON_TRANSLATE = {
+    "0": (True, True),
+    "1": (False, True),
+    "2": (True, False),
+    "3": (False, False)
+}
+
+class Inputs:
+
+    def __init__(self, accelerometer: str, buttons: str, switches: str):
+        self.accelerometer: str = accelerometer
+        self.buttons: tuple[bool] = _BUTTON_TRANSLATE.get(buttons, (False, False))
+        self.switches: int = int(switches)
+        
+
 class FPGAController:
 
     def __init__(self, **kwargs):
@@ -9,8 +24,8 @@ class FPGAController:
         self._jtag_uart.write(f"{command} {argument}".encode())
         return self._jtag_uart.read().decode()
     
-    def read_inputs(self) -> tuple:
-        return self._send_command("r").split("|")
+    def read_inputs(self) -> Inputs:
+        return Inputs(*self._send_command("r").split("|"))
     
     def update_leds(self, num: int):
         self._send_command("l", str(min(num, 9)))
