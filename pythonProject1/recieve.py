4089b57f29ea1d8c894490d081328d7fc8a8acc2
mxwlc <94302084+mxwlc@users.noreply.github.com>
2023-03-21 15:45:51 +0000

    led read

--- pythonProject1/recieve.py
+++ pythonProject1/recieve.py
@@ -6,15 +6,20 @@ This recieves the inputs from the fpga and turns it into keyboard outputs.
 def main():
     fpga = host.FPGAController()
     keyboard = controller.keyboard_controller()
-    
-    ## name
+    fpga.update_hex("player")
     
     
     while True:
+        lives = open("life.txt", "r")
+        current_life = int(lives.read())
+        lives.close()
+        
+        fpga.update_leds(current_life)
+        
         current = fpga.read_inputs()
         keyboard.left_or_right(int(current.accelerometer,16))
         if current.buttons == (True,True) or current.buttons == (False, True):
             keyboard.shoot()
-        
+
 if __name__ == "__main__":
     main()
